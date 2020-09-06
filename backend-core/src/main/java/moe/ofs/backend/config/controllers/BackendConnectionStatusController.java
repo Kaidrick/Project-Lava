7089826fa893923db9df683eb898f3b58599c58a
Jiutian Cai <cjt422@gmail.com>
2020-09-06 23:01:01 +0800

    added player count to ConnectionStatus. added some todos and fixmes to be done in the near future.

--- backend-core/src/main/java/moe/ofs/backend/config/controllers/BackendConnectionStatusController.java
+++ backend-core/src/main/java/moe/ofs/backend/config/controllers/BackendConnectionStatusController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import moe.ofs.backend.BackgroundTask;
 import moe.ofs.backend.config.model.ConnectionStatus;
 import moe.ofs.backend.domain.ExportObject;
+import moe.ofs.backend.domain.PlayerInfo;
 import moe.ofs.backend.util.ConnectionManager;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RestController
 public class BackendConnectionStatusController {
 
-    private AtomicInteger exportObjectCount = new AtomicInteger(0);
-    private AtomicInteger inGamePlayerCount = new AtomicInteger(0);
+    private final AtomicInteger exportObjectCount = new AtomicInteger(0);
+
+    // TODO: change to "in-game" player count in the future, because ED webGui already has this info
+    private final AtomicInteger connectedPlayerCount = new AtomicInteger(0);
 
     @RequestMapping(value = "/status", method = RequestMethod.GET)
     public ConnectionStatus getBackendStatus() {
@@ -31,18 +34,43 @@ public class BackendConnectionStatusController {
         status.setPhaseCode(BackgroundTask.getCurrentTask().getPhase().getStatusCode());
         status.setTheater(BackgroundTask.getCurrentTask().getTaskDcsMapTheaterName());
         status.setObjectCount(exportObjectCount.get());
+        status.setPlayerCount(connectedPlayerCount.get());
 
         return status;
     }
 
-    //    @JmsListener(destination = "unit.spawncontrol", containerFactory = "jmsListenerContainerFactory",
-//            selector = "type = 'spawn'")\
     @JmsListener(destination = "unit.spawncontrol", containerFactory = "jmsListenerContainerFactory",
             selector = "type = 'spawn'")
-    public void receive(ObjectMessage message) throws JMSException {
+    public void receiveUnitSpawnMessage(ObjectMessage message) throws JMSException {
         Serializable object = message.getObject();
         if(object instanceof ExportObject) {
             exportObjectCount.incrementAndGet();
+        }
+    }
+
+    @JmsListener(destination = "unit.spawncontrol", containerFactory = "jmsListenerContainerFactory", selector = "type = 'depawn'")
+    public void receiveUnitDespawnMessage(ObjectMessage message) throws JMSException {
+        Serializable object = message.getObject();
+        if(object instanceof ExportObject) {
+            exportObjectCount.decrementAndGet();
+        }
+    }
+
+    @JmsListener(destination = "player.connection", containerFactory = "jmsListenerContainerFactory",
+            selector = "type = 'connect'")
+    private void logPlayerConnect(ObjectMessage objectMessage) throws JMSException {
+        Serializable object = objectMessage.getObject();
+        if(object instanceof PlayerInfo) {
+            connectedPlayerCount.incrementAndGet();
+        }
+    }
+
+    @JmsListener(destination = "player.connection", containerFactory = "jmsListenerContainerFactory",
+            selector = "type = 'disconnect'")
+    private void logPlayerDisconnect(ObjectMessage objectMessage) throws JMSException {
+        Serializable object = objectMessage.getObject();
+        if(object instanceof PlayerInfo) {
+            connectedPlayerCount.decrementAndGet();
         }
     }
 }
