b230e7b13bc3be8e8b68f3073f9e589ce983c2a4
Kaidrick <cjt422@gmail.com>
2020-11-26 00:05:57 +0800

    ExportObject GraveyardService interface wip

---
+++ backend-core/src/main/java/moe/ofs/backend/services/GraveyardService.java
@@ -0,0 +1,12 @@
+package moe.ofs.backend.services;
+
+/**
+ * Graveyard is where obsolete ExportObject moves when it is no longer active in the sim environment.
+ * The service can be used to search for killed unit.
+ *
+ * The runtime id is unique in DCS runtime by may be reused by other unit; therefore, a unique id
+ * is
+ */
+public interface GraveyardService {
+
+}
