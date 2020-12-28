62d035ec066c975c71dfe91d55df7c3e9b97d056
wssy001 <peter003@foxmail.com>
2020-12-28 13:18:52 +0800

    1.新增Knife4j，一个API文档插件，其配置文件为APIDocConfig.java 2.将LuaDebugConsoleController作为了一个API文档接口示例 3.添加了相关规则，使@RestControllerAdvice不再拦截API文档相关请求 4.修改了application.properties，默认启用Knife4j的增强功能

---
+++ backend-core/src/main/java/moe/ofs/backend/config/APIDocConfig.java
@@ -0,0 +1,61 @@
+package moe.ofs.backend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @ProjectName: Project-Lava
+ * @ClassName: Knife4jConfig
+ * @Description:
+ * @Author: alexpetertyler
+ * @Date: 2020/12/28
+ * @Version v1.0
+ */
+@Configuration
+@EnableSwagger2WebMvc
+@RequiredArgsConstructor
+public class APIDocConfig implements WebMvcConfigurer {
+    private final Environment environment;
+    //    判断当前环境是否为 '开发'，'测试'环境
+//    private boolean b = StrUtil.containsAnyIgnoreCase(environment.getActiveProfiles()[0], "dev", "test");
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+//                .enable(b)
+                .groupName("Debug")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("moe.ofs.backend.debug.controllers"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("北欧式的简单 ，Tyler997", "https://github.com/Kaidrick/Project-Lava", "");
+        return new ApiInfoBuilder()
+                .contact(contact)
+                .title("Project Lava API Doc")
+                .description("A framework for DCS World and a solution to mission scripting, server management and data analysis, packed with a GUI control panel.")
+                .version("v1.0")
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
--- backend-core/src/main/java/moe/ofs/backend/debug/model/LuaCommand.java
+++ backend-core/src/main/java/moe/ofs/backend/debug/model/LuaCommand.java
@@ -1,12 +1,18 @@
 package moe.ofs.backend.debug.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@ApiModel(value = "LuaCommand", description = "JSON string that contains the string to be loaded in specified Lua state")
 public class LuaCommand {
+    @ApiModelProperty(value = "lua代码", example = "return env.mission.theatre")
     private String luaString;
+    @ApiModelProperty(value = "级别", example = "0")
     private int level;
+    @ApiModelProperty(value = "时间戳", example = "2020-10-15T13:07:52.110Z")
     private LocalDateTime timeStamp;
 }
