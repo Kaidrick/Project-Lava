fe9c56a47fce24624eeb764146808548a669e310
wssy001 <peter003@foxmail.com>
2021-02-05 21:41:05 +0800

    1.添加AdminInfo表 2.修改登录逻辑 3.添加超管密码自动生成

---
+++ unified-http-response/src/main/java/moe/ofs/backend/http/security/dao/AdminInfoDao.java
@@ -0,0 +1,8 @@
+package moe.ofs.backend.http.security.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import moe.ofs.backend.http.security.domain.AdminInfo;
+
+public interface AdminInfoDao extends BaseMapper<AdminInfo> {
+
+}
