5188f7f209c9e57d6d8698fc0bab90a6f140298f
wssy001 <peter003@foxmail.com>
2021-02-09 23:58:48 +0800

    1.更新sql语句

--- backend-data/src/main/java/moe/ofs/backend/dao/AdminInfoDao.java
+++ backend-data/src/main/java/moe/ofs/backend/dao/AdminInfoDao.java
@@ -2,7 +2,18 @@ package moe.ofs.backend.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import moe.ofs.backend.domain.AdminInfo;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface AdminInfoDao extends BaseMapper<AdminInfo> {
 
+    @Select("SELECT role_info.role 'roles' FROM group_role,role_info WHERE group_role.role_id = role_info.id AND " +
+            "group_role.group_id IN ( SELECT user_group.group_id FROM user_group WHERE user_group.user_id = ${id});")
+    List<String> selectRoles(@Param("id") Long id);
+
+    @Select("SELECT group_info.`group` 'groups' FROM user_group,group_info WHERE user_group.group_id = group_info.id " +
+            "AND user_group.user_id = ${id};")
+    List<String> selectGroups(@Param("id") Long id);
 }
