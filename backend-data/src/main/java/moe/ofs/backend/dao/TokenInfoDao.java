d53299e98862da4591815a10623cf8490290f75e
wssy001 <peter003@foxmail.com>
2021-02-25 20:55:05 +0800

    修复sql逻辑错误

--- backend-data/src/main/java/moe/ofs/backend/dao/TokenInfoDao.java
+++ backend-data/src/main/java/moe/ofs/backend/dao/TokenInfoDao.java
@@ -10,10 +10,9 @@ import java.util.List;
 
 public interface TokenInfoDao extends BaseMapper<TokenInfo> {
 
-    //TODO 有问题，无法查找到所需token
     @Select("SELECT token_info.id id,token_info.access_token,token_info.access_token_expire_time,token_info.user_id," +
             "token_info.refresh_token,token_info.refresh_token_expire_time,admin_info.name,admin_info.password " +
-            "FROM token_info inner join admin_info WHERE admin_info.id = token_info.id;")
+            "FROM token_info inner join admin_info WHERE admin_info.id = token_info.user_id;")
     List<TokenInfo> selectTokens();
 
     TokenInfo selectOneByAccessToken(@Param("accessToken") String accessToken);
