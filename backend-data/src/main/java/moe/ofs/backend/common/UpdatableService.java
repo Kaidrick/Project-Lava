b021b6a9e75bdd27d4dd36a465f7edafd6863d29
Jiutian Cai <cjt422@gmail.com>
2021-04-24 17:22:36 +0800

    fixed a problem where compiling the project with JDK 11 would fail with error "cannot access com.sun.beans.introspect.PropertyInfo"; Field#set method is used instead of PropertyDescriptor to adapt to JDK 11 standard.

--- backend-data/src/main/java/moe/ofs/backend/common/UpdatableService.java
+++ backend-data/src/main/java/moe/ofs/backend/common/UpdatableService.java
@@ -2,10 +2,7 @@ package moe.ofs.backend.common;
 
 import moe.ofs.backend.domain.dcs.BaseEntity;
 
-import java.beans.IntrospectionException;
-import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -27,8 +24,16 @@ public interface UpdatableService<T extends BaseEntity> {
     }
 
     /**
-     * Update fields on the record object with values of the fields from the update object
-     * @param record the record object whose fields should be
+     * Update fields on the record object with values of the fields from the update object.
+     * In JDK 8, PropertyDescriptor is used to retrieve the field value from update object and then set to record object.
+     *
+     * When compiling with JDK 11 or later, the constructor of PropertyDescriptor should be invoked
+     * with the first parameter being a String value casting to String; the casting is somehow required to
+     * avoid PropertyInfo access issue.
+     *
+     * In JDK 11, the Field#set method can be used instead.
+     *
+     * @param record the record object whose fields should be updated
      * @param update the update object whose fields will be used to insert to record object
      * @return List for String that contains the fields updated
      */
@@ -41,15 +46,12 @@ public interface UpdatableService<T extends BaseEntity> {
                 // if the field is not null,
                 // FIXME: export object field equality needs to be defined; need to rework parking info binary data
                 if (field.get(update) != null && !field.get(update).equals(field.get(record))) {
-                    PropertyDescriptor propertyDescriptor =
-                            new PropertyDescriptor(field.getName(), record.getClass());
 
-                    // invoke setter method on this field
-                    propertyDescriptor.getWriteMethod().invoke(record, field.get(update));
+                    field.set(record, field.get(update));  // adapt changes to set method in newer JDK
 
                     updatedFields.add(field.getName());
                 }
-            } catch (IllegalAccessException | IntrospectionException | InvocationTargetException e) {
+            } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
         });
